5fb64a82d1037dab057921f40a7bccef805704af
Thomas Maier <t.maier@gingco.net>
2019-08-28 15:54:43 +0200

    Rework FIrebase Setup and Messaging

---
+++ src/firebase.tsx
@@ -0,0 +1,28 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
+}
+
+export const initializeFirebaseApp = () => firebase.initializeApp(firebaseConfig)
+
+export const askForNotificationPermission = (): Promise<void> =>
+  firebase.messaging().requestPermission()
+
+export const getToken = (): Promise<string | null> => {
+  const result: Promise<string | null> = firebase
+    .messaging()
+    .getToken()
+    .then(token => token)
+    .catch(error => {
+      console.log('An error occured retriving the messaging token.', error)
+      return null
+    })
+  return result
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 // IMPORTS
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import firebase, { User } from 'firebase/app'
 import { Grommet } from 'grommet'
-import { initializeFirebaseApp } from './firebase'
+import { initializeFirebaseApp, askForNotificationPermission, getToken } from './firebase'
 
 // Styles
 import './index.css'
@@ -17,12 +17,42 @@ import Login from './Pages/Login/'
 import Home from './Pages/Home/'
 
 //----------------------------------------------------------
-// Firebase App Setup
 initializeFirebaseApp()
 
 //----------------------------------------------------------
 const App: FC = () => {
   const [authenticated, setAuthenticated] = useState<boolean>(false)
+  const [token, setToken] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (!token && authenticated) {
+      askForNotificationPermission()
+        .then(async () => {
+          const token = await getToken()
+          console.log('Token: ', token)
+          setToken(token)
+
+          // Update token in database
+          firebase
+            .database()
+            .ref('/token')
+            .once('value')
+            .then(snapshot => {
+              const oldToken = snapshot.val()
+              if (oldToken !== token) snapshot.ref.set(token)
+            })
+
+          firebase.messaging().onTokenRefresh(() => {
+            firebase
+              .messaging()
+              .getToken()
+              .then(token => setToken(token))
+              .catch(error => console.log('An error occured retriving the refreshed token.', error))
+          })
+        })
+        .catch(error => console.log('No Notification permission granted.', error))
+    }
+  })
 
   firebase.auth().onAuthStateChanged((user: User | null) => {
     if (user && !authenticated) {
